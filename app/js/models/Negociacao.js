870a42cb6287fe04fe57b7cc160bbe50b6a3f7d9
Helton Denis <heltondeniss@gmail.com>
2019-08-28 22:23:19 -0300

    Modeling a negotiation

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        // _ significa que as propriedades não poderão ser acessadas fora da classe
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor; 
+    }
+
+    get data() {
+        
+        return this._data;
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    get valor() {
+
+        return this._valor;
+    }
+
+    get volume() {
+        
+        return this._quantidade * this._valor;
+    }
+}
